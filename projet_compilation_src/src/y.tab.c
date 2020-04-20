055f5f61b8be3264b1adb2a6ba8ccfade5aaef5a
3520097 <apolline.gruaux@etu.upmc.fr>
2020-04-20 15:17:16 +0200

    modification lexico.l et grammar.y , renvoie une erreur sur un commentaire , je ne comprends pas

--- projet_compilation_src/src/y.tab.c
+++ projet_compilation_src/src/y.tab.c
@@ -33,6 +33,7 @@
 
 
 /* Global variables */
+
 /* A completer */
 extern bool stop_after_syntax;
 extern bool stop_after_verif;
@@ -48,7 +49,7 @@ void analyse_tree(node_t root);
 node_t make_node(node_nature nature, int nops, ...);
 /* A completer */
 
-#line 36 "grammar.y"
+#line 37 "grammar.y"
 #ifdef YYSTYPE
 #undef  YYSTYPE_IS_DECLARED
 #define YYSTYPE_IS_DECLARED 1
@@ -61,7 +62,7 @@ typedef union {
     node_t ptr;
 } YYSTYPE;
 #endif /* !YYSTYPE_IS_DECLARED */
-#line 65 "y.tab.c"
+#line 66 "y.tab.c"
 
 /* compatibility with bison */
 #ifdef YYPARSE_PARAM
@@ -101,7 +102,56 @@ extern int YYPARSE_DECL();
 #define TOK_TRUE 261
 #define TOK_FALSE 262
 #define TOK_IDENT 263
-#define TOK_STRING 264
+#define TOK_IF 264
+#define TOK_ELSE 265
+#define TOK_WHILE 266
+#define TOK_FOR 267
+#define TOK_PRINT 268
+#define TOK_AFFECT 269
+#define TOK_GE 270
+#define TOK_LE 271
+#define TOK_GT 272
+#define TOK_LT 273
+#define TOK_EQ 274
+#define TOK_NE 275
+#define TOK_PLUS 276
+#define TOK_MINUS 277
+#define TOK_MUL 278
+#define TOK_DIV 279
+#define TOK_MOD 280
+#define TOK_UMINUS 281
+#define TOK_SEMICOL 282
+#define TOK_COMMA 283
+#define TOK_LPAR 284
+#define TOK_RPAR 285
+#define TOK_LACC 286
+#define TOK_RACC 287
+#define TOK_STRING 288
+#define TOK_DO 289
+#define TOK_THEN 290
+#define TOK_OR 291
+#define TOK_AND 292
+#define TOK_BOR 293
+#define TOK_BXOR 294
+#define TOK_BAND 295
+#define TOK_SRL 296
+#define TOK_SRA 297
+#define TOK_SLL 298
+#define TOK_NOT 299
+#define TOK_BNOT 300
+#define listdecl 301
+#define vardecl 302
+#define ident 303
+#define type 304
+#define listtypedecl 305
+#define decl 306
+#define listinst 307
+#define listinstnonnull 308
+#define inst 309
+#define block 310
+#define expr 311
+#define listparamprint 312
+#define paramprint 313
 #define YYERRCODE 256
 typedef short YYINT;
 static const YYINT yylhs[] = {                           -1,
@@ -134,8 +184,8 @@ static const YYINT yycheck[] = {                          2,
 #ifndef YYDEBUG
 #define YYDEBUG 0
 #endif
-#define YYMAXTOKEN 264
-#define YYUNDFTOKEN 269
+#define YYMAXTOKEN 313
+#define YYUNDFTOKEN 318
 #define YYTRANSLATE(a) ((a) > YYMAXTOKEN ? YYUNDFTOKEN : (a))
 #if YYDEBUG
 static const char *const yyname[] = {
@@ -147,8 +197,14 @@ static const char *const yyname[] = {
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"TOK_VOID","TOK_INT","TOK_INTVAL",
-"TOK_BOOL","TOK_TRUE","TOK_FALSE","TOK_IDENT","TOK_STRING",0,0,0,0,
-"illegal-symbol",
+"TOK_BOOL","TOK_TRUE","TOK_FALSE","TOK_IDENT","TOK_IF","TOK_ELSE","TOK_WHILE",
+"TOK_FOR","TOK_PRINT","TOK_AFFECT","TOK_GE","TOK_LE","TOK_GT","TOK_LT","TOK_EQ",
+"TOK_NE","TOK_PLUS","TOK_MINUS","TOK_MUL","TOK_DIV","TOK_MOD","TOK_UMINUS",
+"TOK_SEMICOL","TOK_COMMA","TOK_LPAR","TOK_RPAR","TOK_LACC","TOK_RACC",
+"TOK_STRING","TOK_DO","TOK_THEN","TOK_OR","TOK_AND","TOK_BOR","TOK_BXOR",
+"TOK_BAND","TOK_SRL","TOK_SRA","TOK_SLL","TOK_NOT","TOK_BNOT","listdecl",
+"vardecl","ident","type","listtypedecl","decl","listinst","listinstnonnull",
+"inst","block","expr","listparamprint","paramprint",0,0,0,0,"illegal-symbol",
 };
 static const char *const yyrule[] = {
 "$accept : program",
@@ -193,7 +249,7 @@ typedef struct {
 } YYSTACKDATA;
 /* variables for the parser stack */
 static YYSTACKDATA yystack;
-#line 79 "grammar.y"
+#line 106 "grammar.y"
 
 /* A completer et/ou remplacer avec d'autres fonctions */
 node_t make_node(node_nature nature, int nops, ...) {
@@ -227,7 +283,7 @@ void yyerror(node_t * program_root, char * s) {
     exit(1);
 }
 
-#line 231 "y.tab.c"
+#line 287 "y.tab.c"
 
 #if YYDEBUG
 #include <stdio.h>		/* needed for printf */
@@ -430,28 +486,28 @@ yyreduce:
     switch (yyn)
     {
 case 1:
-#line 58 "grammar.y"
+#line 85 "grammar.y"
 	{
             yyval.ptr = make_node(NODE_PROGRAM, 2, yystack.l_mark[-1].ptr, yystack.l_mark[0].ptr);
             *program_root = yyval.ptr;
         }
 break;
 case 2:
-#line 63 "grammar.y"
+#line 90 "grammar.y"
 	{
             yyval.ptr = make_node(NODE_PROGRAM, 2, NULL, yystack.l_mark[0].ptr);
             *program_root = yyval.ptr;
         }
 break;
 case 3:
-#line 70 "grammar.y"
+#line 97 "grammar.y"
 	{ yyval.ptr = NULL; }
 break;
 case 4:
-#line 74 "grammar.y"
+#line 101 "grammar.y"
 	{ yyval.ptr = NULL; }
 break;
-#line 455 "y.tab.c"
+#line 511 "y.tab.c"
     }
     yystack.s_mark -= yym;
     yystate = *yystack.s_mark;
